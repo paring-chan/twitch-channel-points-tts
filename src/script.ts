53de848e452056b4c2850c4ff8162ae6710ac393
파랑이 <parangee9706@gmail.com>
2021-03-06 21:19:05 +0900

    feat: basic irc connection code

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,29 @@
-console.log('test')
+const params = new URLSearchParams(window.location.search)
+
+let shouldReturn = false
+
+for (const i of ['channel']) {
+    if (!params.get(i)) {
+        alert(`Param ${i} is required.`)
+    }
+}
+
+if (!shouldReturn) {
+    (async () => {
+        const tmi = await import('tmi.js')
+
+        const client = tmi.Client({
+            channels: [params.get('channel')!]
+        })
+
+        client.connect()
+
+        client.on('chat', (channel, tags, msg, self) => {
+            if (tags['custom-reward-id']) {
+                console.log(tags['custom-reward-id'])
+            }
+            if (tags['custom-reward-id'] === params.get('reward_id')) {
+            }
+        })
+    })()
+}
