3aa6166c6def4afca01ce2d381c314f1c50eeda2
파랑이 <parangee9706@gmail.com>
2021-03-06 20:43:49 +0900

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,18 +1,22 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    src: {
-      url: '/',
-      static: true,
-      resolve: false
-    }
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
   ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
   packageOptions: {
     /* ... */
   },
